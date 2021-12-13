d81ba32588ea7b431de99a2f0138a67ca5ae3555
Odumz <odumz20@gmail.com>
2021-12-13 12:58:26 +0100

    initial setup

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="title">
+      <h1>Why choose Easybank?</h1>
+      <p>
+        We leverage Open Banking to turn your bank account into your financial
+        hub. Control your finances like never before.
+      </p>
+    </div>
+    <div class="body">
+      <div class="icon"></div>
+      <div class="content">
+        <h1>Online Banking</h1>
+        <p>
+          Our modern web and mobile applications allow you to keep track of your
+          finances wherever you are in the world.
+        </p>
+      </div>
+      <services />
+    </div>
   </div>
 </template>
