36fe55a844778627b759107e81a9fd9228e2f5f7
Odumz <odumz20@gmail.com>
2021-12-19 22:58:03 +0100

    header and button component updated

---
+++ src/components/button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex">
+    <button
+      class="py-3 px-10 text-white dark:text-gray-800 bg-green-500 hover:opacity-75 font-bold rounded-full w-full"
+    >
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "button",
+  props: {
+    msg: String,
+  },
+});
+</script>
