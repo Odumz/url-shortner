9bf7385bb6044f9982f890283f714d94537d4beb
Odumz <odumz20@gmail.com>
2022-01-07 11:40:33 +0100

    scrollto package added

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./main.css";
+import VueScrollTo from "vue-scrollto";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(VueScrollTo, {
+    offset: -50,
+    easing: "ease-out",
+    lazy: true,
+    duration: 1500,
+  })
+  .mount("#app");
